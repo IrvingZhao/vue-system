58e1b9bc88159f4d729a5a56c2ca1f7a73eb9627
zhao jianan <zhaojn1@lenovo.com>
2019-02-11 13:58:34 +0800

    修改插件引入方式

--- example/index.js
+++ example/index.js
@@ -3,11 +3,13 @@ import ElementUI from 'element-ui';
 import 'element-ui/packages/theme-chalk/src/index.scss';
 
 import Platform from "xlb-platform";
+import Plugin from 'xlb-plugin';
 
 import module from '../src/index';
 
 Vue.use(ElementUI);
 Vue.use(Platform);
+Vue.use(Plugin);
 
 let vueConfig = Platform.getVueConfig([
     {
